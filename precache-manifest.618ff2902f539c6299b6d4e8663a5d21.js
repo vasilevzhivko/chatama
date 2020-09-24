1107971769d01fbe60eca63925f5d1ef6093ff63
Zhivko <jivko.vasilev@gmail.com>
2020-09-24 23:22:08 +0300

    Updates

---
+++ precache-manifest.618ff2902f539c6299b6d4e8663a5d21.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23163c7eae41834d490364a2e59b474b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "64c1ee3e44689f49714b",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "738bc3244d4d5aa2ef00",
+    "url": "/static/js/2.5264ddd1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5264ddd1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64c1ee3e44689f49714b",
+    "url": "/static/js/main.ead5cc4e.chunk.js"
+  },
+  {
+    "revision": "3a46b0aa9ea783f7a3e2",
+    "url": "/static/js/runtime-main.7786b9c2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
